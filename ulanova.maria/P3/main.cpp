379be786f811871f92f364dcea32039822461d21
vamaria06-ux <mashaulanova545@gmail.com>
2025-11-11 23:15:34 +0300

    Handling arguments

--- ulanova.maria/P3/main.cpp
+++ ulanova.maria/P3/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char ** argv) {
+    if (argc < 4) {
+        std::cerr << "Not enough arguments\n";
+        return 1;
+    } else if (argc > 4){
+        std::cerr << "Too many arguments\n";
+        return 1;
+    }
+    try {
+        if( argv[1]!='1' && argv[1]!='2'){
+            std::cerr << "Wrong arguments \n";
+            return 1;
+        }
+    } catch (const std::invalid_arguments& e){
+        std::cerr << "First parametr is not a number \n";
+        return 1;
+    }
 }
